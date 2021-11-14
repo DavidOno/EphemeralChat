dfd1a2692c7a147e5d7407af3f5c8e2ae8a40479
DavidOno <DavidOno@users.noreply.github.com>
2021-11-14 23:53:17 +0100

    WIP: Added presence to chatweb

--- chat/lib/chat_web/live/my_room_live.ex
+++ chat/lib/chat_web/live/my_room_live.ex
@@ -7,6 +7,7 @@ defmodule ChatWeb.MyRoomLive do
     topic = "room:" <> room_id
     username = MnemonicSlugs.generate_slug()
     ChatWeb.Endpoint.subscribe(topic)
+    ChatWeb.Presence.track(self(), topic, username, %{})
     {:ok, assign(
       socket,
       room_id: room_id,
@@ -35,6 +36,13 @@ defmodule ChatWeb.MyRoomLive do
   @impl true
   def handle_info(%{event: "new-message", payload: message}, socket) do
     {:noreply, assign(socket, messages: [message])}
+  end
+
+  @impl true
+  def handle_info(%{event: "presence_diff", payload: %{joins: joins, leaves: leaves}}, socket) do
+    join_messages = joins |> Map.keys() |> Enum.map(fn username -> %{uuid: UUID.uuid4(), content: "#{username} joined", username: "system"} end)
+    leave_messages = leaves |> Map.keys() |> Enum.map(fn username -> %{uuid: UUID.uuid4(), content: "#{username} left", username: "system"} end)
+    {:noreply, assign(socket, messages: join_messages ++ leave_messages)}
   end
 
 end
